716cbb8102f4ae36b0f39e4cc337347318939432
Клюев <sorumond@gmail.com>
2019-05-18 18:10:30 +0300

    add html version

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
+'use strict';
+
 let values = [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
     [0, 0, 0, 0],
-    [2, 2, 8, 8]
+    [0, 0, 0, 0],
 ];
 
-// randomValueStart();
+randomValueStart();
+
+function docs() {
+}
+
+const render = () => {
+    for (let i = 0; i < values.length; i++) {
+        for (let k = 0; k < values.length; k++) {
+            document.querySelectorAll('.game__field').innerHTML += `<div class="game__squares game__squares_${values[i][k]}">${values[i][k]}</div>`
+        }
+    }
+
+    document.querySelector('.game__field').innerHTML = `
+        <div class="game__squares game__squares_2">${values[0][0]}</div>
+        <div class="game__squares game__squares_4">${values[0][1]}</div>
+        <div class="game__squares game__squares_8">${values[0][2]}</div>
+        <div class="game__squares game__squares_16">${values[0][3]}</div>
+
+        <div class="game__squares game__squares_32">${values[1][0]}</div>
+        <div class="game__squares game__squares_64">${values[1][1]}</div>
+        <div class="game__squares game__squares_128">${values[1][2]}</div>
+        <div class="game__squares game__squares_256">${values[1][3]}</div>
+
+        <div class="game__squares game__squares_512">${values[2][0]}</div>
+        <div class="game__squares game__squares_1024">${values[2][1]}</div>
+        <div class="game__squares game__squares_2048">${values[2][2]}</div>
+        <div class="game__squares">${values[][]}</div>
+
+        <div class="game__squares">${values[][]}</div>
+        <div class="game__squares">${values[][]}</div>
+        <div class="game__squares">${values[][]}</div>
+        <div class="game__squares">${values[][]}</div>
+    `
+}
+
+render();
 
 console.log(values);
 
@@ -17,24 +54,20 @@ function moveLeft() {
         while (positionNumber <= 3) {
             if(values[i][positionNumber] === 0) {
                 positionNumber++;
-                continue;
             } else if (positionNumber > 0 && values[i][positionNumber - 1] === values[i][positionNumber] && !isMarged) {
                 values[i][positionNumber - 1] *= 2;
                 values[i][positionNumber] = 0;
                 isMarged = true;
                 positionNumber++;
                 isMoved = true;
-                continue;
             } else if (positionNumber > 0 && values[i][positionNumber - 1] === 0 && positionNumber <= 3) {
                 values[i][positionNumber -1] = values[i][positionNumber];
                 values[i][positionNumber] = 0;
                 positionNumber--;
                 isMoved = true;
-                continue;
             }   else if (positionNumber > 0 && values[i][positionNumber - 1] && isMarged) {
                 positionNumber++;
                 isMarged = false;
-                continue;
             } else {
                 positionNumber++;
             }
@@ -55,24 +88,20 @@ function moveUp() {
         while(positionNumber <= 3) {
            if (values[positionNumber][k] === 0) {
                positionNumber++;
-               continue;
            } else if (positionNumber > 0 && values[positionNumber -1][k] === values[positionNumber][k] && !isMarged) {
                values[positionNumber - 1][k] *= 2;
                values[positionNumber][k] = 0;
                isMarged = true;
                positionNumber++;
                isMoved = true;
-               continue;
            } else if(positionNumber > 0 && values[positionNumber - 1][k] === 0 && positionNumber <= 3) {
                values[positionNumber - 1][k] = values[positionNumber][k];
                values[positionNumber][k] = 0;
                positionNumber--;
                isMoved = true;
-               continue;
            } else if (positionNumber > 0 && values[positionNumber - 1] && isMarged) {
                positionNumber++;
                isMarged = false;
-               continue;
            } else {
                positionNumber++;
            }
@@ -95,24 +124,20 @@ function moveRight() {
         while (positionNumber >= 0) {
             if (values[i][positionNumber] === 0) {
                 positionNumber--;
-                    continue;
             } else if (positionNumber < 3 && values[i][positionNumber] === values[i][positionNumber + 1] && !isMarged) {
                 values[i][positionNumber + 1] *= 2;
                 values[i][positionNumber] = 0;
                 isMarged = true;
                 positionNumber--;
                 isMoved = true;
-                continue;
             } else if (positionNumber < 3 && values[i][positionNumber + 1] === 0 && positionNumber >= 0) {
                 values[i][positionNumber + 1] = values[i][positionNumber];
                 values[i][positionNumber] = 0;
                 positionNumber++;
                 isMoved = true;
-                continue;
             } else if (positionNumber < 3 && isMarged && values[i][positionNumber + 1]) {
                 positionNumber--;
                 isMarged = false;
-                continue;
             } else {
                 positionNumber--;
             }
@@ -137,24 +162,20 @@ function moveDown() {
             // debugg0er;
             if (values[positionNumber][k] === 0) {
                 positionNumber--;
-                continue;
             } else if (positionNumber < 3 && values[positionNumber][k] === values[positionNumber + 1][k] && !isMarged) {
                 values[positionNumber + 1][k] *= 2;
                 values[positionNumber][k] = 0;
                 isMarged = true;
                 positionNumber--;
                 isMoved = true;
-                continue;
             } else if (positionNumber < 3 && values[positionNumber + 1][k] === 0 && positionNumber >= 0) {
                 values[positionNumber + 1][k] = values[positionNumber][k];
                 values[positionNumber][k] = 0;
                 positionNumber++;
                 isMoved = true;
-                continue;
             } else if (positionNumber < 3 && isMarged && values[positionNumber + 1][k]) {
                 positionNumber--;
                 isMarged = false;
-                continue;
             } else {
                 positionNumber--;
             }
@@ -169,4 +190,3 @@ function moveDown() {
         addRandomValue();
     }
 }
-
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
