f27f644b127e1001077939ad81e25e5277608222
Sorumond <sorumond@gmail.com>
2019-05-22 01:31:42 +0300

    add preventDefault

--- touchEvent.js
+++ touchEvent.js
@@ -1,7 +1,8 @@
 let gameField = document.querySelector('.game__field');
 
 gameField.addEventListener('touchstart', touchStart, false);
-gameField.addEventListener('touchend', touchMove, false);
+gameField.addEventListener('touchmove', touchMove, false);
+gameField.addEventListener('touchend', touchUp, false);
 
 let xDown = 0;
 let yDown = 0;
@@ -10,9 +11,14 @@ function touchStart(evt) {
     const firstTouch = evt.touches[0];
      xDown = firstTouch.clientX;
      yDown = firstTouch.clientY;
+     evt.preventDefault();
 };
 
 function touchMove(evt) {
+    evt.preventDefault();
+}
+
+function touchUp(evt) {
     if (!xDown || !yDown) {
         return;
     }
@@ -38,4 +44,5 @@ function touchMove(evt) {
     }
     xDown = 0;
     yDown = 0;
+    evt.preventDefault();
 };
